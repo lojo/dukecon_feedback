f27099adb9cc291df3be933d10d41701bb601fdb
Tim Riemer <tim.riemer@gmx.de>
2018-03-12 14:10:30 +0100

    Initial commit of dukecon-feedback service based on Java 9, Spring Boot 2.0 and JUnit 5

---
+++ src/main/java/org/dukecon/feedback/DukeconFeedbackApplication.java
@@ -0,0 +1,12 @@
+package org.dukecon.feedback;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DukeconFeedbackApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DukeconFeedbackApplication.class, args);
+    }
+}
---
+++ src/test/java/org/dukecon/feedback/DukeconFeedbackApplicationTests.java
@@ -0,0 +1,15 @@
+package org.dukecon.feedback;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+
+@ExtendWith(SpringExtension.class)
+@SpringBootTest
+public class DukeconFeedbackApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+}
