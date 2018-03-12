f484764bae3b803e18278b0a6679f35e99f96324
Tim Riemer <tim.riemer@gmx.de>
2018-03-12 17:08:54 +0100

    PostMapping for feedback including SpringRestDoc tests

--- src/main/java/org/dukecon/feedback/DukeconFeedbackApplication.java
+++ src/main/java/org/dukecon/feedback/DukeconFeedbackApplication.java
@@ -1,7 +1,13 @@
 package org.dukecon.feedback;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 @SpringBootApplication
 public class DukeconFeedbackApplication {
@@ -9,4 +15,20 @@ public class DukeconFeedbackApplication {
     public static void main(String[] args) {
         SpringApplication.run(DukeconFeedbackApplication.class, args);
     }
+
+    @RestController
+    private static class FeedbackController {
+        @PutMapping("talk/{talkId}")
+        public ResponseEntity postFeedback(@PathVariable("talkId") String talkId, @RequestBody FeedbackInput feedbackInput) {
+            return ResponseEntity.status(HttpStatus.CREATED).build();
+        }
+    }
+}
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+class FeedbackInput {
+    private int rating;
+    private String comment;
 }
