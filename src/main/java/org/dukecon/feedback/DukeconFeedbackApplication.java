4dbcf4ae1cd8a1e5fe3480986799dc88bd9d123a
Gerd Aschemann <gerd@aschemann.net>
2018-03-25 17:35:53 +0200

    Replace star imports by single imports

--- src/main/java/org/dukecon/feedback/DukeconFeedbackApplication.java
+++ src/main/java/org/dukecon/feedback/DukeconFeedbackApplication.java
@@ -7,7 +7,11 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
 public class DukeconFeedbackApplication {
