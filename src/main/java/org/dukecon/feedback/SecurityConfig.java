212f986a9a9911aa71cc89f8f5284a9dd8494051
Gerd Aschemann <gerd@aschemann.net>
2018-04-11 20:31:02 +0200

    #1 Start to prepare KeyCloak integration
    Rename ActuatorSecurity config to a more general SecurityConfig

--- src/main/java/org/dukecon/feedback/SecurityConfig.java
+++ src/main/java/org/dukecon/feedback/SecurityConfig.java
@@ -6,7 +6,7 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
 @Configuration
-public class ActuatorSecurity extends WebSecurityConfigurerAdapter {
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
