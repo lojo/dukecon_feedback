a114b220e66dc8427fce4788f161599b3def3d06
Tim Riemer <tim.riemer@gmx.de>
2018-04-21 17:33:54 +0200

    add @Size to Feedback entity properties corresponding to Flyway migration script

--- src/main/java/org/dukecon/feedback/domain/model/Feedback.java
+++ src/main/java/org/dukecon/feedback/domain/model/Feedback.java
@@ -4,16 +4,21 @@ import lombok.Builder;
 import lombok.Data;
 import lombok.NonNull;
 
+import javax.validation.constraints.Size;
+
 @Builder
 @Data
 public class Feedback {
     @NonNull
+    @Size(max = 255)
     private String author;
     @NonNull
+    @Size(max = 255)
     private ConferenceId conferenceId;
     @NonNull
+    @Size(max = 255)
     private TalkId talkId;
-
+    @Size(max = 4096)
     private String comment;
     private Integer rating;
 }
